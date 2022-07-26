ca0a8252f2d440ea324055367f0cd97faba7f927
Karlos-Daniel <68357442+Karlos-Daniel@users.noreply.github.com>
2022-07-25 23:27:53 -0500

    rutas torneos
    "/torneos" crear
    "/crear/torneo" añadir
    "buscar/torneo/:id" obtener torneo por id
    "/borrar/torneo/:id" borrar
    "/editar/torneo/:id" actualizar

---
+++ src/routes/torneos.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { methods as torneosController } from "../controllers/torneos.controller";
+const router = Router();
+
+router.get("/torneos",torneosController.getTorneos);
+router.post("/crear/torneo",torneosController.addTorneo);
+router.get("buscar/torneo/:id",torneosController.getTorneo);
+router.delete("/borrar/torneo/:id",torneosController.deleteTorneo);
+router.put("/editar/torneo/:id",torneosController.updateTorneo);
