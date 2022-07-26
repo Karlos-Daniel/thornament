5878427e44e43e04f7e38153429d0dd5ca30f9b8
Karlos-Daniel <68357442+Karlos-Daniel@users.noreply.github.com>
2022-07-26 00:50:10 -0500

    fix rutas equipos
    fix rutas equipos

--- src/controllers/equipos.controller.js
+++ src/controllers/equipos.controller.js
@@ -4,7 +4,7 @@ import {getConnection} from "./../database/database";
 const getEquipos=async(request,response)=>{
     try{
         const connection=await getConnection();
-        const result=await connection.query("SELECT * FROM equipos");
+        const result=await connection.query("SELECT * FROM equipo");
         console.log(result);
         response.json(result);
     }catch(error){
@@ -33,7 +33,7 @@ const addEquipo=async(request,response)=>{
         
         const connection=await getConnection();
         const result = 
-            await connection.query(`INSERT INTO torneo (juego_idjuego,nombre) 
+            await connection.query(`INSERT INTO equipo (juego_idjuego,nombre) 
             VALUE(${juego},"${nombre}");`);    
     response.json(result);
     }catch(error){
@@ -60,7 +60,7 @@ const updateEquipo=async(request,response)=>{
         const {juego,nombre}=request.body;
         const connection=await getConnection();
         const result = 
-            await connection.query(`UPDATE torneo SET juego_idjuego=${juego},nombre=${nombre}`);
+            await connection.query(`UPDATE equipo SET juego_idjuego=${juego},nombre=${nombre}`);
     
         response.json(result);
     }catch(error){
